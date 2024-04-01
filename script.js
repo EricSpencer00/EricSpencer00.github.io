5f15d469926b4445ae7884881b4ab28b408cd63b
EricSpencer00 <119877507+EricSpencer00@users.noreply.github.com>
2024-03-31 19:56:18 -0500

    Terminal Design change

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ function secretFunction() {
     }, 7000); // 7 seconds
 }
 
+function typeText(text, id, speed) {
+    const container = document.getElementById(id);
+    let index = 0;
+
+    function type() {
+        const char = text[index++];
+        container.textContent += char;
+        if (index < text.length) {
+            setTimeout(type, speed);
+        }
+    }
+
+    type();
+}
+
+
+
