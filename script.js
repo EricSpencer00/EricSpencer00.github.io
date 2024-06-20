3835ee086c1fc63c829434691241ef3570496cdf
EricSpencer00 <119877507+EricSpencer00@users.noreply.github.com>
2024-06-20 14:03:15 -0500

    Chess fcn created

--- script.js
+++ script.js
@@ -29,6 +29,25 @@ function typeText(text, id, speed) {
     type();
 }
 
+function chessStats() { 
+    document.addEventListener('DOMContentLoaded', function() {
+        const username = 'EricSpencer00'
+        const statsDiv = document.getElementById('stats');
+
+        fetch(`https://api.chess.com/pub/player/${username}/stats`)
+            .then(response => response.json())
+            .then(data => {
+                const { chess_blitz, chess_bullet, chess_rapid, chess_daily } = data;
+                const statsHTML = `
+                <h2>Chess.com Stats for ${username}</h2>
+                <p><strong>Blitz:</strong> ${chess_blitz.last.rating}</p>
+                <p><strong>Bullet:</strong> ${chess_bullet.last.rating}</p>
+                <p><strong>Rapid:</strong> ${chess_rapid.last.rating}</p>
+            `;
+                statsDiv.textContent = `Rating: ${stats.last.rating}, Wins: ${stats.record.win}, Losses: ${stats.record.loss}, Draws: ${stats.record.draw}`;
+            });
+}
+
 
 
 
